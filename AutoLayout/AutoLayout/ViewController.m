39572a099b75153fda6b2395716299ef61b3d08c
Kyla <kylag44@gmail.com>
2018-08-10 19:32:15 -0400

    test

---
+++ AutoLayout/AutoLayout/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  AutoLayout
+//
+//  Created by Kyla  on 2018-08-10.
+//  Copyright © 2018 Kyla . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)buttonTapped:(UIButton *)sender
+{
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+        [sender setTitle:@"A very long title for this button"
+                forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
+}
+
+
+
+
+@end
